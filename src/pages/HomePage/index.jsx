fa8bbe1e496af5f0386691d7fba8b515b39355b0
LL <liaposhchenkol@gmail.com>
2024-01-07 11:18:09 +0100

    first commit

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,13 @@
+import './style.css';
+
+export const HomePage = () => {
+  return (
+    <div className="container">
+      <header></header>
+      <main></main>
+      <footer>
+        <p>Czechitas, Digitální akademie: Web</p>
+      </footer>
+    </div>
+  );
+};
